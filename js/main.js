76c4af2ade1ff635cb0a010196d217a98b5dc57c
serch <sponce87@gmail.com>
2018-07-28 12:40:00 -0500

    agregando countDown para la seccion de cuenta regresiva del index

--- js/main.js
+++ js/main.js
@@ -164,4 +164,13 @@ $(function() {
     $('.resumen-evento li:nth-child(2) p').animateNumber({number:15},1200);
     $('.resumen-evento li:nth-child(3) p').animateNumber({number:3},1500);
     $('.resumen-evento li:nth-child(4) p').animateNumber({number:9},1500);
+
+    //Cuenta regresiva
+
+    $('.cuenta-regresiva').countdown('2018/08/02 07:35:00', function(event){
+        $('#dias').html(event.strftime('%D'));
+        $('#horas').html(event.strftime('%H'));
+        $('#minutos').html(event.strftime('%M'));
+        $('#segundos').html(event.strftime('%S'));
+    });
 });
